92396329e2d73ebd2114980c7ad31bb919310bcc
Александр <taxinirvanaa@yandex.ru>
2022-02-04 21:26:38 +0500

    raz

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //входные данные
+        int balance = 100;
+        int payment = 2000;
+
+        //логика
+
+        int bonus = payment / 100;
+        if (payment > 1000)
+            System.out.println("Ваш бонус: " + bonus);
+
+        int balance1 = balance + payment + bonus;
+        System.out.println("Ваш общий баланс: " + balance1);
+
+    }
+}
