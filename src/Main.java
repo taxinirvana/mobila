50c1824412fc20dc72bd0d0be3db8809c12e7ba4
Александр <taxinirvanaa@yandex.ru>
2022-02-07 09:31:20 +0500

    bonus

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,20 @@ public class Main {
     public static void main(String[] args) {
         //входные данные
         int balance = 100;
-        int payment = 701;
+        int payment = 3000;
 
         //логика
-        int bonus = payment / 100;
-        if (payment <= 1000) {
+        int bonus;
+        if (payment >= 1000) {
+             bonus = payment / 100;
+
+        }  else {
             bonus = 0;
         }
-            System.out.println("Ваш бонус: " + bonus);
-
         int balance1 = balance + payment + bonus;
 
-        System.out.println("Ваш общий баланс: " + balance1);
+            System.out.println("Ваш бонус: " + bonus);
+            System.out.println("Ваш общий баланс: " + balance1);
 
 
 
